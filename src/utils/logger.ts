775e2e90d754725fb240b46d9bce98ec4a757bdf
Ryan-Willpower <surasak.c.work@gmail.com>
2021-03-14 01:44:44 +0700

    feat: add more logging utils

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,6 +5,27 @@ export const logger = winston.createLogger({
   transports: [new winston.transports.Console()],
 });
 
-export function printStartStatusToConsole() {
+export function printStartStatusToConsole(error?: Error) {
+  if (error) {
+    logger.error("‼️ Failed to start the server ‼️");
+    logger.error(`‼️ Error name: ${error.name} ‼️`);
+    logger.error(`‼️ Error message: ${error.message} ‼️`);
+
+    if (process.env.NODE_ENV !== "prod") {
+      console.error(error);
+    }
+
+    return;
+  }
+
   logger.info("✨ Server is started at http://localhost");
+}
+
+export function logError(error: Error) {
+  logger.error(`!!! Error name: ${error.name}`);
+  logger.error(`!!! Request Error: ${error.message}`);
+
+  if (process.env.NODE_ENV !== "prod") {
+    console.log(error);
+  }
 }
