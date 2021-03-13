03956b6aeebfbab6118187cd3e7ded8f343ec02a
Ryan-Willpower <surasak.c.work@gmail.com>
2021-03-14 02:42:32 +0700

    feat: add random number util

---
+++ src/utils/randomNumber.ts
@@ -0,0 +1,8 @@
+/**
+ * Return a random number
+ */
+export function getRandomNumber(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
