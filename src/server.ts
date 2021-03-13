e75bbb3b572923a4eaee55755d81544fa5cd00e8
Ryan-Willpower <surasak.c.work@gmail.com>
2021-03-14 01:43:53 +0700

    feat: add user route / seperate mongo for unit test

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,26 @@
 import express from "express";
 import cors from "cors";
+import { json, urlencoded } from "body-parser";
 
 import healthCheckRouter from "./routes/healthcheck";
+import userRouter from "./routes/users";
 import { requestLoggingMiddleware } from "./utils/middleware";
-import { connectToMongo } from "./utils/mongoose";
 
 export function initialServer() {
   const app = express();
 
   app.use(cors());
+  app.use(json());
+  app.use(
+    urlencoded({
+      extended: true,
+    })
+  );
 
   app.use(requestLoggingMiddleware());
 
-  connectToMongo();
-
   app.use("/", healthCheckRouter);
+  app.use("/user", userRouter);
 
   return app;
 }
